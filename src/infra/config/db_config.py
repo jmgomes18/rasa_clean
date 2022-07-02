8f4f3461644e9eea4ad8638c52cc02eb24651560
João Marco Gomes <j.marcogomes18@gmail.com>
2022-07-02 19:12:33 -0300

    config: setup database connection

---
+++ src/infra/config/db_config.py
@@ -0,0 +1,33 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class DBConnectionHandler:
+    """SQLAlchmy database connection"""
+
+    def __init__(self) -> None:
+        self.__user = os.environ.get("POSTGRES_USER")
+        self.__password = os.environ.get("POSTGRES_PASSWORD")
+        self.__host = os.environ.get("POSTGRES_HOST")
+        self.__port = os.environ.get("POSTGRES_PORT")
+        self.__database = os.environ.get("POSTGRES_DATABASE")
+        self.__connection_string = f"postgresql://{self.__user}:{self.__password}@{self.__host}:{self.__port}/{self.__database}?sslmode=require"  # pylint: disable=max-line-length
+        self.session = None
+
+    def get_engine(self):
+        """Return connection Engine
+        :parram - None
+        :return - engine connection to Database
+        """
+        engine = create_engine(self.__connection_string)
+        return engine
+
+    def __enter__(self):
+        engine = create_engine(self.__connection_string)
+        session_maker = sessionmaker()
+        self.session = session_maker(bind=engine)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()  # pylint: disable=no-member
---
+++ src/infra/entities/fields.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Boolean, Column, String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql.sqltypes import DateTime
+from sqlalchemy.sql.sqltypes import Integer
+from src.infra.config import Base
+from datetime import datetime
+import uuid
+
+
+class Fields(Base):
+    __tablename__ = "fields"
+    __table_args__ = {"schema": "rasa"}
+    __hidden_fields__ = {"owner_id"}
+    __resource_name__ = {"singular": "field", "plural": "fields"}
+
+    id = Column("id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    owner_id = Column(String(100), nullable=False)
+    title = Column(String(50), nullable=False)
+    description = Column(String(100), nullable=False)
+    active = Column(Boolean, default=True, nullable=False)
+    type = Column(String(20), nullable=False)
+    order = Column(Integer, nullable=False)
+    created_at = Column("created_at", DateTime, default=datetime.now)
+    updated_at = Column(
+        "updated_at", DateTime, default=datetime.now, onupdate=datetime.now
+    )
+
+    def __repr__(self) -> str:
+        return f"Field: [owner_id={self.owner_id}, title={self.title}, description={self.description}, active={self.active}, type={self.type}, orfer={self.order}]"
+
+    def __eq__(self, __other: object) -> bool:
+        if (
+            self.owner_id == __other.owner_id
+            and self.title == __other.title
+            and self.description == __other.description
+            and self.active == __other.active
+            and self.type == __other.type
+            and self.order == __other.order
+        ):
+            return True
+        return False
